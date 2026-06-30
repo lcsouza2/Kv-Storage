084753514217b90384c61df546d5deb47c8c3a6e
lcsouza2 <lscouzagarcia@gmail.com>
2026-06-30 10:29:20 -0300

    feat: add max_key and min_key to Memtable type for O(1) search

--- include/memtable.h
+++ include/memtable.h
@@ -2,16 +2,18 @@
 #define AVL_TREE_H
 #endif
 
-typedef struct AVLNode {
+typedef struct avl_node {
     char *value;
     char *key;
     int height;
-    struct AVLNode *left;
-    struct AVLNode *right;
+    struct avl_node *left;
+    struct avl_node *right;
 } AVLNode;
 
-typedef struct Memtable {
+typedef struct memtable {
     AVLNode *root;
+    char *min_key;
+    char *max_key;
     int bytes_allocated;
 } Memtable;
 
@@ -19,6 +21,6 @@ Memtable *create_memtable();
 
 Memtable *insert_memtable(Memtable *tree, char *key, char *value);
 
-void memtable_traverse_in_order(AVLNode *node, void (*callback)(AVLNode *));
+void memtable_traverse_in_order(AVLNode *node, void (*callback)(AVLNode *, void *), void *context);
 
 void free_memtable(Memtable *tree);
