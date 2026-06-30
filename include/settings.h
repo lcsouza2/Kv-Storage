559c0a40ca236f46aeaebc4dd813d3ab7d8a9336
lcsouza2 <lscouzagarcia@gmail.com>
2026-06-30 10:31:13 -0300

    feat: add create data storage func signature to settings.h

--- include/settings.h
+++ include/settings.h
@@ -1,6 +1,12 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+#endif
+
 #define MAX_SSTABLE_LEVEL_FILES 5
 #define MAX_SSTABLE_LEVELS 7
 #define MAX_SSTABLE_L0_FILE_SIZE 10485760 // 10MB
 #define SSTABLE_MAX_PATH_LENGTH 256
 
-char *get_sstable_path();
+void get_sstable_path(char *buffer, size_t buffer_size);
+void get_wal_path(char *buffer, size_t buffer_size);
+int create_data_storage_directory();
